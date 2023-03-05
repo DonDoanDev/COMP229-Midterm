0906880d7854fb112585feb229fec2cf956d8883
DonDoanDev <ddoan10@my.centennialcollege.ca>
2023-03-04 20:16:48 -0500

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://127.0.0.1:27017/favor_books" 
+  "URI":"mongodb+srv://donny:BoBocute2103@mongodbserver.g0qbqs5.mongodb.net/favor_books?retryWrites=true&w=majority"
+};
